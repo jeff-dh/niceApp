1a2c17e0c56b36d5d967c65607dd69346f8c7926
jeff <1105041+jeff-dh@users.noreply.github.com>
2025-05-01 10:03:51 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from nicegui import ui, app
 
+def is_android():
+    import platform
+    return "android" in platform.uname().release
+
 @ui.page("/")
 def index():
-    def is_android():
-        import platform
-        return "android" in platform.uname().release
-
     if is_android():
         ui.label("Hello nice android world")
-    else:
-        ui.label("Hello World")
 
     storage = app.storage.user
     storage["foobar"] = storage.get("foobar", 0)
@@ -18,7 +16,6 @@ def index():
         storage["foobar"] += 1
         ui.label(f"yeah{'.'*storage['foobar']}")
 
-    # ui.button("Really?", on_click=lambda: ui.label("yeah"))
     ui.button("Really?", on_click=callback)
 
 ui.run(host='localhost', reload=False, show=False, storage_secret="foobar")
